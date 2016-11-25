466ccee9252dddb6e8028b28e90490438eaff0ac
Paulina Friemann <paulina@friemann.com>
2016-11-25 11:24:43 +0100

    moving left and right

--- persuasion/main.py
+++ persuasion/main.py
@@ -10,6 +10,7 @@ class Agent:
         self.color = color
         self.width = 5
         self.height = 5
+        self.speed = [0,0]
 
     def get_rect(self):
         return pygame.Rect(self.x - self.width / 2,
@@ -23,23 +24,8 @@ class Agent:
     def move(self, speed):
         self.set_rect(self.get_rect().move(speed))
 
-
-class Player(Agent):
-
-    def __init__(self, x, y, color, screen):
-        Agent.__init__(self, x, y, color)
-        self.speed = [0, 0]
-        self.screen = screen
-
-    def moveup(self):
-        self.speed = [0, -1]
-
-    def stop(self):
-        self.speed = [0, 0]
-
     def update(self):
         self.move(self.speed)
-        pygame.event.pump()
 
 
 def main():
@@ -52,8 +38,8 @@ def main():
 
     screen = pygame.display.set_mode(size)
 
-    agents = [Agent(width/2, height - 20, white)]
-    player = Player(width/2, height - 20, white, screen)
+    #agents = [Agent(width/2, height - 20, white)]
+    player = Agent(width/2, height - 20, white)
 
     clock = pygame.time.Clock()
 
@@ -61,23 +47,20 @@ def main():
 
         clock.tick(30)
 
+        pressed = pygame.key.get_pressed()
+
+        player.speed[1] = - int(pressed[pygame.K_UP])
+        player.speed[0] = int(pressed[pygame.K_RIGHT]) - int(pressed[pygame.K_LEFT])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP or event.key == pygame.K_w:
-                    player.moveup()
+        player.update()
 
-            #elif event.type == pygame.KEYUP:
-             #   if event.key == pygame.K_UP:
-              #      player.stop()
+        screen.fill(black)
 
-            player.update()
-
-            screen.fill(black)
-
-            pygame.draw.rect(screen, player.color, player.get_rect())
-            pygame.display.flip()
+        pygame.draw.rect(screen, player.color, player.get_rect())
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
