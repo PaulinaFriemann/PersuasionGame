3da27c64949f10924f2dbcf688fa9c826a4fcd78
Paulina Friemann <paulina@friemann.com>
2016-12-20 14:02:48 +0100

    merge

--- persuasion/agents.py
+++ persuasion/agents.py
@@ -2,23 +2,13 @@ import pygame
 import movements
 import behaviors
 import math
+from enum import Enum
 
 
-class Sensor:
-
-    def __init__(self, personal_space, center):
-        self.radius = personal_space
-        self.center = center
-
-    def update(self, rect):
-        self.center = rect.center
-
-    def distance(self, rect):
-        center_rect = rect.center
-        return math.sqrt((center_rect[0] - self.center[0]) ** 2 + (center_rect[1] - self.center[1]) ** 2)
-
-    def direction(self, rect):
-        return [a - b for a,b in zip(self.center, rect.center)]
+class Attitude(Enum):
+    neutral = 0
+    avoiding = 1
+    friendly = 2
 
 
 class Agent:
@@ -29,7 +19,6 @@ class Agent:
         self.rect = pygame.Rect(x - 6 / 2, y - 6 / 2, 6, 6)
         self.speed_modificator = 1
         self.personalspace = 20
-        self.sensor = Sensor(self.personalspace, self.rect.center)
         self.movement = movement
         self.behavior = behavior
         self.player = player
@@ -45,11 +34,13 @@ class Agent:
 
         if behavior == behaviors.avoid:
             self.runaway = False
+
+    def direction_to(self, rect):
+        return [a - b for a,b in zip(self.rect.center, rect.center)]
     
     def move(self, speed):
         speed = map(lambda x: self.speed_modificator * x, speed)
         self.rect = self.rect.move(speed)
-        self.sensor.update(self.rect)
 
     def update(self):
         self.behavior(self)
--- persuasion/behaviors.py
+++ persuasion/behaviors.py
@@ -13,7 +13,7 @@ def make_happy(agent):
 def avoid(agent):
     if not agent.runaway and agent.distance_to_player <= agent.personalspace:
         agent.runaway = True
-        direction = agent.sensor.direction(agent.player.rect)
+        direction = agent.direction_to(agent.player.rect)
         print direction
         agent.speed = [int(round(dir / agent.distance_to_player)) for dir in direction]
         print agent.speed
