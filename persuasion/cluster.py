29aaeb6f94f2e6ed50e968d43ef1ee066f1d571e
Marc <m.j.m.degroot@student.ru.nl>
2016-12-25 18:25:56 +0100

    [progress] Very Ugly Way Of Creating Circular Cluster
    Please forgive me.
    
    def create_cluster(self, position, shape, amount, exampleAgent, game):
    
    position - (center x, center y, diameter)
    shape - Shape.circle
    amount - amount of agents
    ExampleAgent - agent to use as example.
    game - the game to add the agents to.

--- persuasion/cluster.py
+++ persuasion/cluster.py
@@ -1,5 +1,7 @@
 import agents
 import movements
+import math
+import random
 from enum import Enum
 from agents import Attitude
 
@@ -12,18 +14,31 @@ class Shape(Enum):
 class Cluster:
 
 
-    def __init__(self,members,clustno):
+    def __init__(self,clustno,members = [],):
         self.members = members
         self.clustno = clustno
 
-    def create_cluster(self, position, shape, amount, exampleAgent):
-        if shape == Shape.circle :
-            #dosomething
-            print "something"
+    def distance(self, (x,y), (x2,y2)):
+        return math.sqrt((x - x2) ** 2 + (y - y2) ** 2)
 
-        for i in range(0,amount):
-            self.members[i] = agents.Agent(exampleAgent.x , exampleAgent.y, exampleAgent.color, exampleAgent.screen, exampleAgent.movement, exampleAgent.attitude, exampleAgent.player)
+    def create_cluster(self, position, shape, amount, exampleAgent, game):
+        amountleft = amount
+        agent_locations = []
+        if shape == Shape.circle:
+        try:
+                for x in range(position[0] - position[2],position[0] + position[2]):
+                    for y in range(position[1] - position[2], position[1] + position[2]):
+                        if amountleft > 0:
+                            if random.randint(0, amountleft) > 0:
+                                if distance((x,y),(position[0],position[1])) <= position[2]:
+                                    agent_locations.append([(x,y)])
+                                    amountleft = amountleft - 1
+                                    raise NoMoreAgents
 
+        except NoMoreAgents:
+            for i in range(0,len(locations)):
+                self.members[i] = agents.Agent(agent_locations[i][0] , agent_locations[i][1], exampleAgent.color, exampleAgent.screen, exampleAgent.movement, exampleAgent.attitude, exampleAgent.player)
+                game.add_agent(self.members[i])
 
     def add_member(self,member):
         self.members.extend(member)
